3d3a253a4bfb5a6da7f177975f55315afeae9c06
KAMRAN <Kami.zare74@gmail.com>
2025-02-21 16:46:09 +0330

    Add project files.

---
+++ Basket/Basket/Program.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Basket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("order.create", false, false, false, null);
+            string message = $"Send Shopping cart information to place an order Time{DateTime.Now.Second}";
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish("", routingKey: "order.create", null, body);
+
+            //در ورودی BasicPublish 
+            // است Direct را خالی ارسال کنیم یعنی نوع آن Exchange نام
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Order/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("order.create", false, false, false, null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, eventArgs) =>
+            {
+                var body = eventArgs.Body.ToArray();
+                string Msg = Encoding.UTF8.GetString(body);
+                Console.WriteLine("Data" + Msg);
+                channel.BasicAck(eventArgs.DeliveryTag,true);
+            };
+            channel.BasicConsume("order.create",false,consumer);
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
